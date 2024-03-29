c3b782399b99696189def2a270dee7ad8a42f740
bkleinb78 <y2bk728@aol.com>
2024-03-29 16:56:45 -0500

    decided to revamp the calculator and rewrote the project + completed it as well

--- Level 1 task 3 - Calculator/index.js
+++ Level 1 task 3 - Calculator/index.js
@@ -1,18 +1,36 @@
-let string = "";
-let buttons = document.querySelectorAll(".button");
+const switchbtn = document.getElementById("switch");
+const display = document.getElementById("display");
+const input = document.querySelectorAll("input");
+let value = "";
+switchbtn.addEventListener("click", () => {
+  if (switchbtn.checked == true) {
+    document.body.setAttribute("data-theme", "dark");
+  } else {
+    document.body.setAttribute("data-theme", "");
+  }
+});
 
-Array.from(buttons).forEach((button) => {
-  button.addEventListener("click", (e) => {
-    if (e.target.innerHTML == "=") {
-      string = eval(string);
-      document.querySelector("input").value = string;
-    } else if (e.target.innerHTML == "C") {
-      string = "";
-      document.querySelector("input").value = string;
+input.forEach((e) => {
+  e.addEventListener("click", (event) => {
+    if (event.target.value == "=") {
+      if (value.length != 0) {
+        let newval = eval(value);
+        value = newval;
+        display.value = value;
+      }
+    } else if (event.target.value == "C") {
+      value = "";
+      display.value = value;
+    } else if (event.target.value == "switch") {
     } else {
-      console.log(e.target);
-      string = string + e.target.innerHTML;
-      document.querySelector("input").value = string;
+      value += event.target.value;
+      display.value = value;
+    }
+    if (!event.target.classList.contains("switch")) {
+      event.target.classList.add("active");
+      setTimeout(() => {
+        event.target.classList.remove("active");
+      }, 400);
     }
   });
 });
